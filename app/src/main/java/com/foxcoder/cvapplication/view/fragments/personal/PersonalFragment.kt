4dbf7cbce562defd8ec29ee55d7cf064b8870c90
ray <rayhan.khan@gozayaan.com>
2021-05-02 00:19:47 +0600

    feat: personal list view item added

--- app/src/main/java/com/foxcoder/cvapplication/view/fragments/personal/PersonalFragment.kt
+++ app/src/main/java/com/foxcoder/cvapplication/view/fragments/personal/PersonalFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.foxcoder.cvapplication.R
 import com.foxcoder.cvapplication.databinding.FragmentPersonalBinding
+import com.foxcoder.cvapplication.utils.setLightStatusBar
 
 class PersonalFragment : Fragment() {
 
@@ -22,6 +23,7 @@ class PersonalFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setLightStatusBar(view,requireActivity())
         initViewItems()
         super.onViewCreated(view, savedInstanceState)
     }
