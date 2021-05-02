72b0113e4343518a7b200200eeecc42ed94e8eee
ray <rayhan.khan@gozayaan.com>
2021-05-03 02:18:12 +0600

    feat: override on created method

--- app/src/main/java/com/foxcoder/cvapplication/view/base_classes/BaseDialogFragment.kt
+++ app/src/main/java/com/foxcoder/cvapplication/view/base_classes/BaseDialogFragment.kt
@@ -1,6 +1,32 @@
 package com.foxcoder.cvapplication.view.base_classes
 
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
-class BaseDialogFragment : DialogFragment() {
+open class BaseDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+
+        dialog.setOnShowListener { dialogInterface ->
+            val sheetDialog = dialogInterface as? BottomSheetDialog
+            val bottomSheet = sheetDialog?.findViewById<FrameLayout>(
+                com.google.android.material.R.id.design_bottom_sheet
+            )
+
+            bottomSheet?.let {
+                val behavior = BottomSheetBehavior.from(it)
+                behavior.skipCollapsed = true
+                behavior.isFitToContents = true
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+            }
+        }
+        return dialog
+    }
+
 }
