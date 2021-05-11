e50d8690890933bd5e64586e4db25401619b5725
ray <rayhan.khan@gozayaan.com>
2021-05-12 02:25:10 +0600

    feat: add language dialog

---
+++ app/src/main/java/com/foxcoder/cvapplication/view/fragments/personal/adapters/LanguageDialog.kt
@@ -0,0 +1,30 @@
+package com.foxcoder.cvapplication.view.fragments.personal.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.foxcoder.cvapplication.R
+import com.foxcoder.cvapplication.databinding.FragmentLanguageDialogBinding
+import com.foxcoder.cvapplication.view.base_classes.BaseDialogFragment
+
+
+class LanguageDialog : BaseDialogFragment() {
+
+    private var _binding: FragmentLanguageDialogBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLanguageDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
